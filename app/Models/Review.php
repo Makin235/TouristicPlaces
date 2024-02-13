d71924c0a032e615e319999a8ce3be760769c18c
Makin235 <abdelhamid.makin@gmail.com>
2024-02-13 22:10:14 +0300

    Models are updated.

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'place_id',
+        'user_id',
+        'IP',
+        'subject',
+        'review',
+        'rate',
+    ];
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
